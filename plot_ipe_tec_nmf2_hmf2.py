ddaa97177fae7f5f3fbc2947ff29ca21676878cf
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2020-06-13 02:46:47 +0000

    first steps for parallelization

--- plot_ipe_tec_nmf2_hmf2.py
+++ plot_ipe_tec_nmf2_hmf2.py
@@ -5,16 +5,19 @@ Filled contours
 An example of contourf on manufactured data.
 
 """
+from __future__ import print_function
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib
+matplotlib.use('Agg')
 import numpy as np
 from netCDF4 import Dataset
-
+import glob
 import cartopy
 import cartopy.crs as ccrs
-
-
+from datetime import datetime,timedelta
+from multiprocessing import Pool
 
 def ncdump(nc_fid, verb=True):
     '''
@@ -48,74 +51,77 @@ def ncdump(nc_fid, verb=True):
             a valid netCDF4.Dataset.variables key
         """
         try:
-            print "\t\ttype:", repr(nc_fid.variables[key].dtype)
+            print("\t\ttype:", repr(nc_fid.variables[key].dtype))
             for ncattr in nc_fid.variables[key].ncattrs():
-                print '\t\t%s:' % ncattr,\
-                      repr(nc_fid.variables[key].getncattr(ncattr))
+                print('\t\t%s:' % ncattr,\
+                      repr(nc_fid.variables[key].getncattr(ncattr)))
         except KeyError:
-            print "\t\tWARNING: %s does not contain variable attributes" % key
+            print("\t\tWARNING: %s does not contain variable attributes" % key)
 
     # NetCDF global attributes
     nc_attrs = nc_fid.ncattrs()
     if verb:
-        print "NetCDF Global Attributes:"
+        print("NetCDF Global Attributes:")
         for nc_attr in nc_attrs:
-            print '\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr))
+            print('\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr)))
     nc_dims = [dim for dim in nc_fid.dimensions]  # list of nc dimensions
     # Dimension shape information.
     if verb:
-        print "NetCDF dimension information:"
+        print("NetCDF dimension information:")
         for dim in nc_dims:
-            print "\tName:", dim 
-            print "\t\tsize:", len(nc_fid.dimensions[dim])
+            print("\tName:", dim) 
+            print("\t\tsize:", len(nc_fid.dimensions[dim]))
             print_ncattr(dim)
     # Variable information.
     nc_vars = [var for var in nc_fid.variables]  # list of nc variables
     if verb:
-        print "NetCDF variable information:"
+        print("NetCDF variable information:")
         for var in nc_vars:
             if var not in nc_dims:
-                print '\tName:', var
-                print "\t\tdimensions:", nc_fid.variables[var].dimensions
-                print "\t\tsize:", nc_fid.variables[var].size
+                print('\tName:', var)
+                print("\t\tdimensions:", nc_fid.variables[var].dimensions)
+                print("\t\tsize:", nc_fid.variables[var].size)
                 print_ncattr(var)
     return nc_attrs, nc_dims, nc_vars
 
 
-def main():
-
-    input_output_path = "/Users/georgemillward/Downloads/"
+def plot(i):
+    # get these from argument parameters
+    input_output_path = "/scratch1/NCEPDEV/stmp4/Adam.Kubaryk/opsdat"
+    start_date = datetime(2020,6,1,3,0,0)
+    current_date = start_date + timedelta(minutes=(i+1)*3)
+    timestamp = datetime.strftime(current_date,"%Y%m%d_%H%M%S")
+    file = "wfs.t06z.ipe03.{}.nc".format(timestamp)
 
     fig = plt.figure(figsize=(10, 6))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree(central_longitude=0))
     fig.subplots_adjust(left=0.05, right=0.95, bottom=0., top=0.9)
 
-    # nc_fid = Dataset("/Users/georgemillward/Downloads/ipe.20150317_151500.nc", "r", format="NETCDF4")
-    nc_fid = Dataset(input_output_path+"ipe.20130316_010000_nmf2.nc", "r", format="NETCDF4")
+    nc_fid = Dataset("{}/{}".format(input_output_path,file), "r", format="NETCDF4")
     nc_attrs, nc_dims, nc_vars = ncdump(nc_fid)
     for var in nc_vars:
         if var not in nc_dims:
-            print '\tName:', var
-            print "\t\tdimensions:", nc_fid.variables[var].dimensions
-            print "\t\tsize:", nc_fid.variables[var].size
+            print('\tName:', var)
+            print("\t\tdimensions:", nc_fid.variables[var].dimensions)
+            print("\t\tsize:", nc_fid.variables[var].size)
             #print_ncattr(var)
     lon = nc_fid.variables['lon'] # longitude
     lat = nc_fid.variables['lat'] # latitude
     tec = nc_fid.variables['tec'] # TEC
-    nmf2 = nc_fid.variables['nmf2'] # nmf2
-    hmf2 = nc_fid.variables['hmf2'] # nmf2
+    nmf2 = nc_fid.variables['NmF2'] # nmf2
+    hmf2 = nc_fid.variables['HmF2'] # nmf2
     
     latvals = lat[:]
-    print 'latvals'
-    print latvals
+    print('latvals')
+    print(latvals)
 
     lonvals = lon[:]
-    print 'lonvals'
-    print lonvals
+    print('lonvals')
+    print(lonvals)
 
     tecvals = tec[:]
-    print 'tecvals'
-    print tecvals
+    print('tecvals')
+    print(tecvals)
 
     nmf2vals = nmf2[:]
     hmf2vals = hmf2[:]
@@ -140,7 +146,7 @@ def main():
 
     # plt.show()
 
-    plt.savefig(input_output_path+"tec.png")
+    plt.savefig("{}/tec_{}.png".format(input_output_path,timestamp))
     tec_title.remove()
     # contour_plot.remove()
 
@@ -153,7 +159,7 @@ def main():
     cb.set_label('NmF2') 
 
     nmf2_title = ax.text(0.5,1.05,'WAM-IPE nmf2 (m-3)',fontsize=16,transform=ax.transAxes,horizontalalignment='center')
-    plt.savefig(input_output_path+"nmf2.png")
+    plt.savefig("{}/nmf2_{}.png".format(input_output_path,timestamp))
     nmf2_title.remove()
     # contour_plot.remove()
 
@@ -165,21 +171,14 @@ def main():
     cb = mpl.colorbar.ColorbarBase(ax2,cmap=cmap3,orientation='vertical')
     cb.set_label('hmF2')
     hmf2_title = ax.text(0.5,1.05,'WAM-IPE hmf2 (km)',fontsize=16,transform=ax.transAxes,horizontalalignment='center')
-    plt.savefig(input_output_path+"hmf2.png")
+    plt.savefig("{}/hmf2_{}.png".format(input_output_path,timestamp))
+    plt.close()
 
-
-# Calculate MUF(3000)
-# loctime = Local Time (0-23:59 corresponding to the longitude) This changes with UT!
-# nlongs = number of longitudes
-# nlats = number of latitudes
-# for i=0, nlongs do begin
-# for j=0, nlats do begin
-# M3000(i,j) = 1490./(hmF2(i,j) + 176.)
-# M3000(i,j) = M3000(i,j) - .6sin(3.1415/12.(loctime(i) - 5.))
-# MUF3000(i,j) = M3000(i,j)*sqrt(nmF2(i,j))/(1.11355287e+5)
-# endfor ; j=0,90
-# endfor ;i=0,20
-
+def main():
+    # paralellize!
+    plot(1)
+#    with multiprocessing.Pool(processes=10) as pool:
+#        pool.map(plot, range(306))
 
 if __name__ == '__main__':
     main()
