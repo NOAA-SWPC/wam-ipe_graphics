01488b14c871f7071763fe19a3e572d50a36a881
george <george@MacBook-Pro.lan>
2020-06-24 14:08:01 -0600

    a few comments....

--- plot4.py
+++ plot4.py
@@ -84,6 +84,9 @@ def ncdump(nc_fid, verb=True):
 
 
 def main():
+
+	# netcdf bit....
+
 	nc_fid = Dataset("/Users/george/Downloads/ipe.20130316_010000_nmf2.nc", "r", format="NETCDF4")
 	# nc_fid = Dataset("/Users/george/Downloads/IPE_Ne.geo.202006041900.nc4", "r", format="NETCDF4")
 	nc_attrs, nc_dims, nc_vars = ncdump(nc_fid)
@@ -103,6 +106,8 @@ def main():
 	print("NmF2 max: {:.2e}".format(nmf2vals.max()))
 	print("HmF2 max: {:.1f}".format(hmf2vals.max()))
 
+	# color maps
+
 	cmap = plt.get_cmap('Blues',256)
 	cmap2 = plt.get_cmap('Reds',256)
 	cmap3 = plt.get_cmap('Greens',256)
@@ -117,6 +122,8 @@ def main():
 	# background_color = (0.1,0.1,0.1)
 	background_color = (0.9,0.9,0.9)
 
+	# the main figure
+
 	fig=plt.figure(figsize=(16,9),facecolor=background_color)
 
 	main_title_size = 20
@@ -125,13 +132,20 @@ def main():
 	main_title = fig.text(0.04,0.76,'Model: WAM-IPE',fontsize=20,horizontalalignment='left',color=text_color)
 	main_title = fig.text(0.02,0.025,'Space Weather Prediction Center',fontsize=16,horizontalalignment='left',color=text_color)
 
+	# the cartopy projection
+
 	projection = ccrs.PlateCarree(central_longitude=0)
+
+	# sub plots - 2 x 2 grid
+
 	the_plots = fig.subplots(2, 2, subplot_kw=dict(projection=projection))
 	fig.subplots_adjust(left=0.03, right=0.98, bottom=0.02, top=0.98, wspace=0.01, hspace=0.0)
 	nticks = 5
 	nticks_tec = 6
 	title_size = 14
 	n_contours = 20
+
+	# plot [0,0] set to invisible
 
 	ax0 = the_plots[0,0]
 	ax0.set_visible(False)
@@ -142,9 +156,13 @@ def main():
 	min_nmf2 = 0.0
 	landcolor = (1,1,1)
 
+	# TEC plot top right
+
 	ax1 = the_plots[0,1]
 	contour_plot = ax1.contourf(lonvals, latvals, tecvals, np.linspace(min_tec,max_tec,n_contours), extend='max', transform=ccrs.PlateCarree(central_longitude=0), cmap=cmap)
 	tec_title = ax1.text(0.5,1.05,'Total Electron Content (TEC)',fontsize=title_size,transform=ax1.transAxes,horizontalalignment='center',color=text_color)
+
+	# all the colorbar stuff
 
 	cax, kw = mpl.colorbar.make_axes(ax1,cmap=cmap,pad=0.03,shrink=0.6)
 	cb1=fig.colorbar(contour_plot,cax=cax,ticks=np.linspace(min_tec,max_tec,nticks_tec),**kw)
@@ -152,13 +170,23 @@ def main():
 	cb1.outline.set_edgecolor(edge_color)
 	plt.setp(plt.getp(cb1.ax.axes, 'yticklabels'), color=text_color)
 
+	# coastlines and continents
+
 	ax1.coastlines(alpha=0.1)
 	ax1.add_feature(cfeature.LAND, facecolor = landcolor, zorder=3, alpha=0.1)
+
+	# the 'white' gridlines'
+
 	ax1.gridlines(alpha=0.3,draw_labels=True)
+
+	# right lables and top labels should not be there - not working ....
+
 	ax1.xlabels_top = False
 	ax1.ylabels_right = False
 	ax1.xlines = False
 	ax1.set_global()
+
+	# NMF2 plot is bottom left
 
 	ax2 = the_plots[1,0]
 	contour_plot2 = ax2.contourf(lonvals, latvals, nmf2vals, np.linspace(min_nmf2,max_nmf2,n_contours), extend='max', transform=ccrs.PlateCarree(central_longitude=0), cmap=cmap2)
@@ -174,6 +202,8 @@ def main():
 	ax2.add_feature(cfeature.LAND, facecolor = landcolor, zorder=3, alpha=0.1)
 	ax2.gridlines(alpha=0.3)
 	ax2.set_global()
+
+	# HMF2 plot bottom right
 
 	ax3 = the_plots[1,1]
 	contour_plot3 = ax3.contourf(lonvals, latvals, hmf2vals, np.linspace(200,1000,n_contours), extend='both', transform=ccrs.PlateCarree(central_longitude=0), cmap=cmap3)
